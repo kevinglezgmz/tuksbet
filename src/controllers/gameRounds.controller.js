65a0faf468c59a64fecd326d46bb0d7e86264d99
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-10-16 16:59:02 -0500

    feat: implemented basic game rounds CRU (deletion must not be supported) operations in the gameRounds controller

--- src/controllers/gameRounds.controller.js
+++ src/controllers/gameRounds.controller.js
@@ -1,9 +1,69 @@
+const { ObjectID } = require('bson');
 const Database = require('../models/database.model.js');
 
 /**
  * @typedef { import('./dataTypes').GameRound } GameRound
  */
 
-class GameRoundsController {}
+class GameRoundsController {
+  static getAllGameRounds(req, res) {
+    const gameRoundsDb = new Database('GameRounds');
+    gameRoundsDb
+      .find({}, {})
+      .toArray()
+      .then((results) => {
+        if (results.length === 0) {
+          res.status(400).send({ msg: 'No game rounds found!' });
+        } else {
+          res.status(200).send({ data: results });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err });
+      });
+  }
+
+  static getGameRoundById(req, res) {
+    const gameRoundsDb = new Database('GameRounds');
+    gameRoundsDb
+      .findOne({ _id: ObjectID(req.params.gameRoundId) }, {})
+      .then((result) => {
+        if (result) {
+          res.status(200).send({ data: result });
+        } else {
+          res.status(500).send({ err: 'Game round with id ' + req.params.gameRoundId + ' does not exist' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err });
+      });
+  }
+
+  static createGameRound(req, res) {
+    const gameRoundsDb = new Database('GameRounds');
+    /** @type { GameRound } */
+    const gameRoundData = req.body;
+    gameRoundsDb
+      .insertOne(gameRoundData)
+      .then((result) => {
+        res.send({ status: result });
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
+
+  static updateGameRound(req, res) {
+    const gameRoundsDb = new Database('GameRounds');
+    gameRoundsDb
+      .updateOne({ _id: ObjectID(req.params.gameRoundId) }, { $set: req.body })
+      .then((result) => {
+        res.send({ status: result });
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
+}
 
 module.exports = GameRoundsController;
