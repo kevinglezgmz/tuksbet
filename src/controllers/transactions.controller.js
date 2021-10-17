e782f10b6aaf6ef6358e162c55266003f1251044
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-10-16 23:37:12 -0500

    feat: implemented basic transactions CRU (deleting must not be implemented) operations in the transactions controller

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -1,9 +1,69 @@
+const { ObjectID } = require('bson');
 const Database = require('../models/database.model.js');
 
 /**
  * @typedef { import('./dataTypes').Transaction } Transaction
  */
 
-class TransactionsController {}
+class TransactionsController {
+  static getAllTransactions(req, res) {
+    const transactionsDb = new Database('Transactions');
+    transactionsDb
+      .find({}, {})
+      .toArray()
+      .then((results) => {
+        if (results.length === 0) {
+          res.status(400).send({ msg: 'No transactions found!' });
+        } else {
+          res.status(200).send({ data: results });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err });
+      });
+  }
+
+  static getTransactionById(req, res) {
+    const transactionsDb = new Database('Transactions');
+    transactionsDb
+      .findOne({ _id: ObjectID(req.params.transactionId) }, {})
+      .then((result) => {
+        if (result) {
+          res.status(200).send({ data: result });
+        } else {
+          res.status(400).send({ err: 'Transaction ' + req.params.transactionId + ' does not exist' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err });
+      });
+  }
+
+  static createTransaction(req, res) {
+    const transactionsDb = new Database('Transactions');
+    /** @type { Transaction } */
+    const transactionData = req.body;
+    transactionsDb
+      .insertOne(transactionData)
+      .then((result) => {
+        res.send({ status: result });
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
+
+  static updateTransaction(req, res) {
+    const transactionsDb = new Database('Transactions');
+    transactionsDb
+      .updateOne({ _id: ObjectID(req.params.transactionId) }, { $set: req.body })
+      .then((result) => {
+        res.send({ status: result });
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
+}
 
 module.exports = TransactionsController;
