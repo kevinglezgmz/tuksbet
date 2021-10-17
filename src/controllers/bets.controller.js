9ed1fdfe1378d5eb43b62d99b4abdd60f55b5dd9
Kevin González <44679178+kevinglezgmz@users.noreply.github.com>
2021-10-16 23:35:15 -0500

    feat: implemented basic chats CR (updating and deleting are subject to change) operations in the chats controller (#5)
    feat: implemented basic bets CR (updating and deleting are subject to change) operations in the chats controller

--- src/controllers/bets.controller.js
+++ src/controllers/bets.controller.js
@@ -1,9 +1,57 @@
+const { ObjectID } = require('bson');
 const Database = require('../models/database.model.js');
 
 /**
  * @typedef { import('./dataTypes').BetHistory } BetHistory
  */
 
-class BetsController {}
+class BetsController {
+  static getAllBets(req, res) {
+    const betsDb = new Database('BetHistory');
+    betsDb
+      .find({}, {})
+      .toArray()
+      .then((results) => {
+        if (results.length === 0) {
+          res.status(400).send({ msg: 'No bets have been made yet!' });
+        } else {
+          res.status(200).send({ data: results });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err });
+      });
+  }
+
+  static getBetById(req, res) {
+    const betsDb = new Database('BetHistory');
+    betsDb
+      .findOne({ _id: ObjectID(req.params.betId) }, {})
+      .then((result) => {
+        if (result) {
+          res.status(200).send({ data: result });
+        } else {
+          res.status(500).send({ err: 'Bet with Id ' + req.params.betId + ' does not exist' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err });
+      });
+  }
+
+  static createBet(req, res) {
+    const betsDb = new Database('BetHistory');
+    /** @type { BetHistory } */
+    const betData = req.body;
+    betsDb
+      .insertOne(betData)
+      .then((result) => {
+        res.send({ status: result });
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
+}
 
 module.exports = BetsController;
--- src/routes/bets.js
+++ src/routes/bets.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
+const BetsController = require('../controllers/bets.controller.js');
 
-router.get('/', (req, res) => {
-  res.send('Bets route works');
-});
-router.get('/:betId', (req, res) => {
-  res.send('Bets with id: ' + req.params.betId + ' route works');
-});
-router.post('/', (req, res) => {});
-router.post('/:id', (req, res) => {});
+router.get('/', BetsController.getAllBets);
+router.get('/:betId', BetsController.getBetById);
+router.post('/', BetsController.createBet);
 
 module.exports = router;
