491e9d98c98e411514db5ecd184a286ee32a3a85
Miguel Gonzalez Barajas <is722298@iteso.mx>
2021-10-29 13:50:25 -0500

    feat: added swagger documentation for Users

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -17,12 +17,12 @@ module.exports = router;
  * /api/users/:
  *  get:
  *    tags:
- *      - Bets
+ *      - Users
  *    summary: GET request for all the users
  *    description: Make a server request to get all the users that have been registered.
  *    responses:
  *      200:
- *        description: Success response. Retrieves all the data of the bets.
+ *        description: Success response. Retrieves all the data of the users.
  *        schema:
  *          type: object
  *          properties:
@@ -34,27 +34,18 @@ module.exports = router;
  *                  _id:
  *                    type: string
  *                    example: 616b9a868562c959c04a3cd1
- *                  betId:
- *                    type: int
- *                    example: 136
- *                  userId:
- *                    type: int
- *                    example: 32
- *                  gameRoudId:
- *                    type: int
- *                    example: 13254
- *                  betDate:
- *                    type: date
- *                    example: Wed Jan 27 2021 10:15:53 GMT+1000 (AEST)
- *                  betAmount:
- *                    type: float
- *                    example: 0.56
- *                  betPayout:
- *                    type: float
- *                    example: -0.56
- *                  betStake:
- *                    type: string 
- *                    example: "roulette"
+ *                  username:
+ *                    type: string
+ *                    example: exampleUser123
+ *                  email:
+ *                    type: string
+ *                    example: "example@example.com"
+ *                  password:
+ *                    type: string
+ *                    example: examplePasswordEncrypted123
+ *                  balance:
+ *                    type: float 
+ *                    example: 12.34
  *      400:
  *        description: Bad request
  *        schema:
@@ -62,7 +53,7 @@ module.exports = router;
  *          properties:
  *            err:
  *              type: string
- *              example: No bets.
+ *              example: No users.
  *      401:
  *        description:
  *        schema:
@@ -78,54 +69,258 @@ module.exports = router;
  *          properties:
  *            err:
  *              type: string
- *              example: Bad request
+ *              example: Unexpected error, please try again
  */
 
-// GET BET BY ID
+// GET USER BY ID
 /**
  * @swagger
- * /api/bets/:betId:
+ * /api/users/{userId}:
  *  get:
  *    tags:
- *      - Bets
- *    summary:
+ *      - Users
+ *    summary: GET request for one user
+ *    description: Make a server request to get one of the users who has been registered.
  *    parameters:
- *    description:
+ *      - in: path
+ *        name: userId
+ *        description: The id of the user required
+ *        example: 617c32b2ee63d136fa191527
  *    responses:
  *      200:
- *        description:
+ *        description: Success response. Retrieves all the data of the user.
  *        schema:
+ *          type: object
+ *          properties:
+ *            data:
+ *              type: object
+ *              properties:
+ *                _id:
+ *                  type: string
+ *                  example: 616b9a868562c959c04a3cd1
+ *                username:
+ *                  type: string
+ *                  example: exampleUser123
+ *                email:
+ *                  type: string
+ *                  example: "example@example.com"
+ *                password:
+ *                  type: string
+ *                  example: examplePasswordEncrypted123
+ *                balance:
+ *                  type: float 
+ *                  example: 12.34
  *      400:
- *        description:
+ *        description: Bad request
  *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: No user.
  *      401:
  *        description:
  *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Bad request
  *      500:
  *        description:
  *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
  */
 
-// POST A BET
+// POST NEW USER
 /**
  * @swagger
- * /api/bets/:
+ * /api/users/:
  *  post:
  *    tags:
- *      - Bets
- *    summary:
- *    description:
+ *      - Users
+ *    summary: POST request for one user
+ *    description: Makes a server request to register a new user
+ *    parameters:
+ *       - in: body
+ *         name: user
+ *         description: The user to be registered
+ *         schema:
+ *           type: object
+ *           required:
+ *             - username
+ *             - email
+ *             - password
+ *             - balance
+ *           properties:
+ *             username:
+ *               type: string
+ *               example: exampleUser2
+ *             email:
+ *               type: string
+ *               example: "example2@example.com"
+ *             password:
+ *               type: string
+ *               example: examplePasswordEncrypted123
+ *             balance:
+ *               type: int
+ *               example: 22.22
  *    responses:
- *      200:
- *        description:
+ *      201:
+ *        description: Success response. Retrieves status of the registration.
  *        schema:
+ *          type: object
+ *          properties:
+ *            msg:
+ *              type: string
+ *              example: User created
  *      400:
- *        description:
+ *        description: Bad request
  *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: No user.
  *      401:
  *        description:
  *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Bad request
  *      500:
  *        description:
  *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
+
+// PATCH USER
+/**
+ * @swagger
+ * /api/users/{userId}:
+ *  patch:
+ *    tags:
+ *      - Users
+ *    summary: PATCH request for one user
+ *    description: Makes a server request to register a new user
+ *    parameters:
+ *       - in: path
+ *         name: userId
+ *         description: The id of the user to be updated
+ *         example: 617c32b2ee63d136fa191527
+ *       - in: body
+ *         name: user
+ *         description: The user to be registered
+ *         schema:
+ *           type: object
+ *           required:
+ *             - username
+ *             - email
+ *             - password
+ *             - balance
+ *           properties:
+ *             username:
+ *               type: string
+ *               example: exampleUser2
+ *             email:
+ *               type: string
+ *               example: "example2@example.com"
+ *             password:
+ *               type: string
+ *               example: examplePasswordEncrypted123
+ *             balance:
+ *               type: int
+ *               example: 13.22
+ *    responses:
+ *      201:
+ *        description: Success response. Retrieves status of the registration.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            msg:
+ *              type: string
+ *              example: User created
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: No user.
+ *      401:
+ *        description:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Bad request
+ *      500:
+ *        description:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
+
+// DELETE USER
+/**
+ * @swagger
+ * /api/users/{userId}:
+ *  delete:
+ *    tags:
+ *      - Users
+ *    summary: DELETE request for one user
+ *    description: Makes a server request to delete a user
+ *    parameters:
+ *      - in: path
+ *        name: userId
+ *        description: The id of the user required
+ *        example: 617c3cd231a62043533f4952
+ *    responses:
+ *      200:
+ *        description: Success response. Retrieves status of the registration.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            msg:
+ *              type: string
+ *              example: User deleted
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: No user.
+ *      401:
+ *        description:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Bad request
+ *      500:
+ *        description:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
  */
