cc34c1ceb77200733148cc1da5f614ce84287cc3
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-10-31 13:17:25 -0600

    feat: implemented sessions routes for users to log in and out

---
+++ server/src/controllers/sessions.controller.js
@@ -0,0 +1,76 @@
+const { getObjectId } = require('../utils');
+const Database = require('../models/database.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const SECRET_JWT = process.env.SECRET_JWT || 'h@la123Cr@yola';
+
+class SessionsController {
+  static loginUser(req, res) {
+    if (!req.body.email && !req.body.password) {
+      res.status(400).send({ msg: 'Missing email or password' });
+      return;
+    }
+
+    const usersDb = new Database('Users');
+    const sessionsDb = new Database('Sessions');
+
+    usersDb
+      .findOne({ email: req.body.email })
+      .then((user) => {
+        return new Promise((success, reject) => {
+          if (!user || !user.email || !bcrypt.compareSync(req.body.password, user.password)) {
+            reject({ statusCode: 400, msg: 'Could not find an user with that email and password combination' });
+          }
+          const token = jwt.sign({ _id: user._id, username: user.username, email: user.email }, SECRET_JWT);
+          success({ user, token });
+        });
+      })
+      .then(({ user, token }) => {
+        return new Promise((success, reject) => {
+          sessionsDb
+            .insertOrUpdateOne(
+              { userId: user._id },
+              {
+                $set: {
+                  token,
+                  userId: user._id,
+                  lastLogin: new Date(),
+                },
+              },
+              { upsert: true }
+            )
+            .then((status) => {
+              if (status.acknowledged) {
+                success({ statusInsert: status, token });
+              } else {
+                reject({ statusCode: 500, msg: 'Unexpected error ocurred, please try again' });
+              }
+            });
+        });
+      })
+      .then(({ statusInsert, token }) => {
+        res.status(200).send({ statusInsert, token });
+      })
+      .catch(({ statusCode, msg }) => {
+        res.status(statusCode).send({ err: msg });
+      });
+  }
+
+  static logoutUser(req, res) {
+    const sessionsDb = new Database('Sessions');
+    sessionsDb
+      .deleteOne({ userId: getObjectId(req.userId) })
+      .then((result) => {
+        if (result.deletedCount > 0) {
+          res.send({ msg: 'Successfully logged out' });
+        } else {
+          res.status(400).send({ err: 'You can not logout if you are not logged in' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ err: 'Unexpected error, please try again' });
+      });
+  }
+}
+
+module.exports = SessionsController;
---
+++ server/src/middlewares/authentication.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const Database = require('../models/database.model');
+const { getObjectId } = require('../utils');
+const SECRET_JWT = process.env.SECRET_JWT || 'h@la123Cr@yola';
+
+function authentication(req, res, next) {
+  /** Extract token from headers */
+  const authHeader = req.get('Authorization');
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    res.status(401).send({ err: 'Not authorized' });
+    return;
+  }
+  const authHeaderToken = authHeader.split(' ')[1] || undefined;
+
+  /** JWT Token validation */
+  let token = undefined;
+  try {
+    token = jwt.verify(authHeaderToken, SECRET_JWT);
+  } catch (err) {
+    res.status(403).send({ err: 'Invalid token' });
+    return;
+  }
+
+  /** Token verification in DB */
+  const sessionsDb = new Database('Sessions');
+  sessionsDb
+    .findOne({ userId: getObjectId(token._id) }, {})
+    .then((sessionRecord) => {
+      if (!sessionRecord || sessionRecord.token !== authHeaderToken) {
+        res.status(401).send({ err: 'Not authorized' });
+        return;
+      }
+      /** Inject userId into the request */
+      req.userId = token._id;
+      next();
+    })
+    .catch((err) => {
+      res.status(statusCode).send({ err: 'Unexpected error, please try again' });
+    });
+}
+
+module.exports = authentication;
