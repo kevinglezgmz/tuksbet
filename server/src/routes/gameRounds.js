91094fb58df457f23b31bef760092232468e0a25
Nataly Salazar <is72034@iteso.mx>
2021-10-31 20:16:00 -0600

    feat: added swagger documentation for game rounds router

--- server/src/routes/gameRounds.js
+++ server/src/routes/gameRounds.js
@@ -7,3 +7,308 @@ router.post('/', GameRoundsController.createGameRound);
 router.patch('/:gameRoundId', GameRoundsController.updateGameRound);
 
 module.exports = router;
+
+// GET ALL GAME ROUNDS
+/**
+ * @swagger
+ * /api/gamerounds/:
+ *  get:
+ *    tags:
+ *      - Game Rounds
+ *    summary: GET request for all the game rounds
+ *    description: Makes a server request to get all the game rounds that exist.
+ *    responses:
+ *      200:
+ *        description: Success response. Retrieves all the data of the game rounds.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            data:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                    example: 616b9a868562c959c04a3cd1
+ *                  gameId:
+ *                    type: int
+ *                    example: 32
+ *                  result:
+ *                    type: string
+ *                    example: abc
+ *                  roundDate:
+ *                    type: date
+ *                    example: Wed Jan 27 2021 10:15:53 GMT+1000 (AEST)
+ *                  acceptingBets:
+ *                    type: boolean
+ *                    example: true
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: No game rounds
+ *      401:
+ *        description: Unauthorized
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Not authorized
+ *      500:
+ *        description: Internal error
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
+
+// GET GAME ROUND BY ID
+/**
+ * @swagger
+ * /api/gamerounds/{gameRoundId}:
+ *  get:
+ *    tags:
+ *      - Game Rounds
+ *    summary: GET request for a specific game round
+ *    description: Makes a server request for a specific game round using it's ID.
+ *    parameters:
+ *      - in: path
+ *        name: gameRoundId
+ *        example: 123
+ *    responses:
+ *      200:
+ *        description: Success response. Retrieves all the data of the game round.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            _id:
+ *              type: string
+ *              example: 616b9a868562c959c04a3cd1
+ *            gameId:
+ *              type: int
+ *              example: 32
+ *            result:
+ *              type: string
+ *              example: abc
+ *            roundDate:
+ *               type: date
+ *               example: Wed Jan 27 2021 10:15:53 GMT+1000 (AEST)
+ *            acceptingBets:
+ *              type: boolean
+ *              example: true
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: No game rounds
+ *      401:
+ *        description: Unauthorized
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Not authorized
+ *      500:
+ *        description: Internal error
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
+
+// POST A NEW GAME ROUND
+/**
+ * @swagger
+ * /api/gamerounds/:
+ *  post:
+ *    tags:
+ *      - Game Rounds
+ *    summary: POST request for a new bet
+ *    description: Makes a server request to post a new game round.
+ *    parameters:
+ *      - in: body
+ *        name: gameRound
+ *        description: The game round to be posted
+ *        schema:
+ *          type: object
+ *          required:
+ *            - gameId
+ *            - result
+ *            - roundDate
+ *            - acceptingBets
+ *          properties:
+ *            gameId:
+ *              type: int
+ *              example: 32
+ *            result:
+ *              type: string
+ *              example: abc
+ *            roundDate:
+ *               type: date
+ *               example: Wed Jan 27 2021 10:15:53 GMT+1000 (AEST)
+ *            acceptingBets:
+ *              type: boolean
+ *              example: true
+ *    responses:
+ *      201:
+ *        description: Success response. Retrieves status of the game round
+ *        schema:
+ *          type: object
+ *          properties:
+ *            msg:
+ *              type: string
+ *              example: Game round added
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Could not post the game round
+ *      401:
+ *        description: Unauthorized
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Not authorized
+ *      500:
+ *        description: Internal error
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
+
+// PATCH A GAME ROUND
+/**
+ * @swagger
+ * /api/gamerounds/{gameRoundId}:
+ *  patch:
+ *    tags:
+ *      - Game Rounds
+ *    summary: PATCH request for a gamee round
+ *    description: Makes a server request to register changes of a game round.
+ *    parameters:
+ *      - in: path
+ *        name: gameRoundId
+ *        example: 123
+ *      - in: body
+ *        name: gameRound
+ *        description: The game round to be posted
+ *        schema:
+ *          type: object
+ *          required:
+ *            - gameId
+ *          properties:
+ *            gameId:
+ *              type: int
+ *              example: 32
+ *            result:
+ *              type: string
+ *              example: abc
+ *            roundDate:
+ *               type: date
+ *               example: Wed Jan 27 2021 10:15:53 GMT+1000 (AEST)
+ *            acceptingBets:
+ *              type: boolean
+ *              example: true
+ *    responses:
+ *      201:
+ *        description: Success response. Retrieves status of the game round
+ *        schema:
+ *          type: object
+ *          properties:
+ *            msg:
+ *              type: string
+ *              example: Game round updated
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Could not update the game round
+ *      401:
+ *        description: Unauthorized
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Not authorized
+ *      500:
+ *        description: Internal error
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
+
+// DELETE GAME ROUND
+/**
+ * @swagger
+ * /api/gamerounds/{gameRoundId}:
+ *  delete:
+ *    tags:
+ *      - Game Rounds
+ *    summary: DELETE request for a specific game round
+ *    description: Makes a server request to delete a specific game round using it's ID.
+ *    parameters:
+ *      - in: path
+ *        name: gameRoundId
+ *        example: 123
+ *    responses:
+ *      200:
+ *        description: Success response. Retrieves status of the bet deletion.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            msg:
+ *              type: string
+ *              example: Game round deleted
+ *      400:
+ *        description: Bad request
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Could not delete the game round
+ *      401:
+ *        description: Unauthorized
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Not authorized
+ *      500:
+ *        description: Internal error
+ *        schema:
+ *          type: object
+ *          properties:
+ *            err:
+ *              type: string
+ *              example: Unexpected error, please try again
+ */
