4ef52968798c9e38b5aa336e071ef291b9ddee57
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-11-01 22:19:54 -0600

    chore: created initial pages and layout components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlackjackComponent } from './pages/blackjack/blackjack.component';
+import { CrashComponent } from './pages/crash/crash.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RouletteComponent } from './pages/roulette/roulette.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'roulette', component: RouletteComponent },
+  { path: 'crash', component: CrashComponent },
+  { path: 'blackjack', component: BlackjackComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RouletteComponent } from './pages/roulette/roulette.component';
+import { CrashComponent } from './pages/crash/crash.component';
+import { BlackjackComponent } from './pages/blackjack/blackjack.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ChatSidebarComponent } from './layout/chat-sidebar/chat-sidebar.component';
+import { DepositComponent } from './pages/deposit/deposit.component';
+import { WithdrawComponent } from './pages/withdraw/withdraw.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    RouletteComponent,
+    CrashComponent,
+    BlackjackComponent,
+    NotFoundComponent,
+    FooterComponent,
+    ChatSidebarComponent,
+    DepositComponent,
+    WithdrawComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
