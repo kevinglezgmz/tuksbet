72fdf2275bf751aa66b592dbd56c92c2b2eca142
MikeGonzaBar <is722298@iteso.mx>
2021-11-07 22:09:50 -0600

    feat: Added the view for the users and user details, deactivated the authentication check for getUserById

---
+++ frontend/src/app/common/data-types/users.ts
@@ -0,0 +1,7 @@
+export interface User {
+  userId: string;
+  username: string;
+  email: string;
+  password?: string;
+  balance?: number;
+}
---
+++ frontend/src/app/pages/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/common/data-types/users';
+import { UserService } from 'src/app/common/services/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+})
+export class UsersComponent implements OnInit {
+  users: User[] = [];
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().then((users: User[]) => {
+      this.users = users.reverse();
+    });
+  }
+}
