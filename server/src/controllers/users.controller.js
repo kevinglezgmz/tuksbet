2350945d42747879206eea799ab8390e202bce5c
MikeGonzaBar <is722298@iteso.mx>
2021-11-07 22:06:21 -0600

    feat: Added the view for the users and user details, deactivated the authentication check for getUserById

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -33,10 +33,10 @@ class UsersController {
   }
 
   static getUserById(req, res) {
-    if (req.userId !== req.params.userId) {
-      res.status(401).send({ err: 'Not authorized' });
-      return;
-    }
+    //if (req.userId !== req.params.userId) {
+    //  res.status(401).send({ err: 'Not authorized' });
+    //  return;
+    // }
 
     const usersDb = new Database('Users');
     usersDb
