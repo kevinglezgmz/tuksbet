01d67428f647d17cc52d153629da8d542c8f3e41
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-11-07 18:20:45 -0600

    feat: added transactions view and updated controller to include user's username in the transaction

--- frontend/src/app/pages/transactions/transactions.component.ts
+++ frontend/src/app/pages/transactions/transactions.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/common/data-types/transaction';
+import { TransactionService } from 'src/app/common/services/transaction.service';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+  styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent implements OnInit {
-
-  constructor() { }
+  transactions: Transaction[] = [];
+  constructor(private transactionService: TransactionService) {}
 
   ngOnInit(): void {
+    this.transactionService.getAllTransactions().then((transactions: Transaction[]) => {
+      this.transactions = transactions.reverse();
+    });
   }
-
 }
