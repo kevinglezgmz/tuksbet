5ac74e0bfe25d44f6c6dc132f6647f26d2458d89
Nataly Salazar <is72034@iteso.mx>
2021-11-10 14:10:43 -0600

    feat: added the game showcase for the dashboard

---
+++ frontend/src/app/pages/dashboard/game-showcase/game-showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-showcase',
+  templateUrl: './game-showcase.component.html',
+  styleUrls: ['./game-showcase.component.scss']
+})
+export class GameShowcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
