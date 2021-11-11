21c015828be7728561ba505aec994ebba98ced08
Nataly Salazar <is72034@iteso.mx>
2021-11-10 23:28:10 -0600

    fix: added col-4, deleted unnecesary margins and console.log()

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -13,7 +13,6 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
     this.gameService.getAllGames().then((games: Game[]) => {
-      console.log(games);
       this.games = games;
     })
   }
