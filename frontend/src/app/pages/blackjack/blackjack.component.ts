5099af062a818f8afe925ad8203871d27ca153bb
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2021-11-25 17:04:06 -0600

    fix: removed unnecessary import

--- frontend/src/app/pages/blackjack/blackjack.component.ts
+++ frontend/src/app/pages/blackjack/blackjack.component.ts
@@ -1,4 +1,3 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
