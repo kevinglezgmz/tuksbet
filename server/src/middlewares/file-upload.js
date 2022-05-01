d2295cc8606fdc9ea3ac9f3a21fdc7c097223d9f
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2022-04-30 23:06:28 -0500

    fix: restored AWS credentials env values

--- server/src/middlewares/file-upload.js
+++ server/src/middlewares/file-upload.js
@@ -3,7 +3,12 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const S3Client = new AWS.S3({ region: process.env.AWS_S3_BUCKET_REGION });
+const S3Client = new AWS.S3({
+  region: process.env.AWS_S3_BUCKET_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  Bucket: process.env.AWS_S3_BUCKET,
+});
 
 const fileFilter = (req, file, cb) => {
   if (
