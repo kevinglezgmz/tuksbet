a231f9ab52c21e0ddbab8311dbcd90f1bc65f562
Kevin Gonzalez <kevingonzalezgmz@gmail.com>
2022-04-30 23:31:18 -0500

    chore: added AWS session token env variable

--- server/src/middlewares/file-upload.js
+++ server/src/middlewares/file-upload.js
@@ -8,6 +8,7 @@ const S3Client = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   Bucket: process.env.AWS_S3_BUCKET,
+  sessionToken: process.env.AWS_SESSION_TOKEN,
 });
 
 const fileFilter = (req, file, cb) => {
